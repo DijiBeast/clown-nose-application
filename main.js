d004364c84be1455362df0051f621b3b72fe356d
DijiBeast <86576666+DijiBeast@users.noreply.github.com>
2022-03-13 18:51:39 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 noseX = 0;
 noseY = 0;
-function preLoad()
+function preload()
 {
     clown_nose = loadImage("clown_nose.png");
 }
@@ -21,8 +21,7 @@ function draw()
 image(video, 0, 0, 500, 500);
 fill("#eb3734");
 stroke("#0a0a0a");
-circle(noseX, noseY, 20);
-image(clown_nose, noseX, noseY, 20, 20);
+image(clown_nose, noseX, noseY, 30, 30);
 }
 
 function take_snapshot()
@@ -40,8 +39,8 @@ function gotPoses(results)
     if (results.length > 0)
     {
         console.log(results);
-        noseX = results[0].pose.nose.x;
-        noseY = results[0].pose.nose.y;
+        noseX = results[0].pose.nose.x - 10;
+        noseY = results[0].pose.nose.y - 10;
 
         console.log("nose x = " + results[0].pose.nose.x);
         console.log("nose y = " + results[0].pose.nose.y);
